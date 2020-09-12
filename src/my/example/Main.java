0b3eec14425c5450a6380ba2e83b83259d1d2d21
chicohendrix <ritviksk@gmail.com>
2020-09-12 04:24:42 +0400

    Initial commit

---
+++ src/my/example/Main.java
@@ -0,0 +1,11 @@
+// Author - Ritvik Kumar Kothapalli
+
+package my.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+    }
+}
